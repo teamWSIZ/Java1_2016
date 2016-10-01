43445c968c6b166137ad503c33c86594f1bec39b
WSIZ <teamWSIZ@users.noreply.github.com>
2016-10-01 15:37:32 +0200

    Create A.java

---
+++ src/A.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * Created by user46 on 10/1/16.
+ */
+public class A {
+    //psvm tab
+    public static void main(String[] args) {
+        //sout tab
+        System.out.println("hello world");
+        int a = 12;
+        ///
+        int b = a * 10;
+
+        String s = "Abra kadabra";
+
+        String ss = s + s;
+
+        System.out.println(b);
+        System.out.println(ss);
+        System.out.println(ss.substring(10));
+        System.out.println(ss.contains("kadabrax"));
+
+        //fori  tab
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(s + i);
+//        }
+//        int[] ttt = new int[10];
+//        for (int i = 0; i < 10; i++) {
+//            ttt[i] = i * i;
+//        }
+//        Arrays.sort(ttt);
+//        System.out.println(Arrays.toString(ttt));
+    }
+}
