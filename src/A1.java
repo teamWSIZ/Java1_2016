cc144f2995452b54b5566b85054a4182bb263e65
WSIZ <teamWSIZ@users.noreply.github.com>
2016-10-07 19:37:01 +0200

    Create A1.java

---
+++ src/A1.java
@@ -0,0 +1,34 @@
+/**
+ * Created on 07.10.16, at 18:09
+ */
+public class A1 {
+    //psvm + [tab]
+    public static void main(String[] args) {
+        //sout + [tab]
+        System.out.println("hi");
+
+        int x = 10;
+        int y = 3;
+        int z = x % y;
+        System.out.println(x);
+        System.out.println("y=" + y);
+        System.out.println("reszta z dzielenia x/y wychodzi " + z);
+
+        System.out.println(6 * 6 * 6 * 6 * 6);
+        int liczba = 17;
+        //czy to jest liczba pierwsza?
+        for (int i = 2; i < liczba; i++) {
+            System.out.println("Reszta z dzielenia " + liczba + " przez " + i + " to: " + (liczba%i));
+            System.out.println("i=" + i);
+        }
+
+        if (x == 10) {
+            System.out.println("x jest rowne 10");
+        } else {
+            System.out.println("x to nie jest 10");
+        }
+
+
+
+    }
+}
