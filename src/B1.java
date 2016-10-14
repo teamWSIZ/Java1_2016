5ecdfbed5b00d6bba38f4e8c778772df9c1cd312
WSIZ <teamWSIZ@users.noreply.github.com>
2016-10-14 21:20:40 +0200

    Update B1.java

--- src/B1.java
+++ src/B1.java
@@ -1,19 +1,16 @@
-/**
- * Created on 14.10.16, at 18:18
- */
 public class B1 {
 
     public static void main(String[] args) {
-//        int x = 1;
-//        int y = 33;
-//
-//        if (x==y) {
-//            System.out.println("x jest równe y");
-//        }
-//        if (x<y) {
-//            System.out.println("x jest mniejsze niż y");
-//        }
-//        //---------------------------------
+        int x = 1;
+        int y = 33;
+
+        if (x==y) {
+            System.out.println("x jest równe y");
+        }
+        if (x<y) {
+            System.out.println("x jest mniejsze niż y");
+        }
+        //---------------------------------
 
 
         int szukanyKod = 12;
@@ -22,13 +19,9 @@ public class B1 {
             System.out.println("Sprawdzam kod dla spr=" + spr);
             if (spr==szukanyKod) {
                 System.out.println("Trafiłem; szukany kod był równy: " + spr);
-            } else {
+            } else  {
                 System.out.println("Nie trafiony");
             }
         }
-
-
-
     }
-
 }
