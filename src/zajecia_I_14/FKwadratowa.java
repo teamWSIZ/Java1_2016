617470031f47cc5e73321436ea3f04873790feba
Stasszek <staszek906@o2.pl>
2017-01-14 14:00:49 +0100

    zajecia 14.01

---
+++ src/zajecia_I_14/FKwadratowa.java
@@ -0,0 +1,53 @@
+package zajecia_I_14;
+
+import zajecia_I_7.Oceny;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by stasz_000 on 2017-01-14.
+ */
+public class FKwadratowa {
+    private JPanel panel1;
+    private JButton button1;
+    private JTextField wartosci;
+    private JTextField wyswietlWinyk;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("FKwadratowa");
+        frame.setContentPane(new FKwadratowa().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public FKwadratowa() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //4,5,2
+                String[] napis = wartosci.getText().split(",");
+                // zamieniamy napisy z tablicy na liczby
+                int a = Integer.valueOf(napis[0]);
+                int b = Integer.valueOf(napis[1]);
+                int c = Integer.valueOf(napis[2]);
+
+                //liczymy delte i wyciągamy pierwiastek (Math.sqrt() liczy na pierwiastek z liczby)
+                double pDt = Math.sqrt(b * b - 4 * a * c);
+                if (pDt > 0) {
+                    double x1 = (-b - pDt) / 2 * a;
+                    double x2 = (-b + pDt) / 2 * a;
+                    wyswietlWinyk.setText("X1 = " + x1 + " X2 = " + x2);
+                } else if (pDt == 0) {
+                    wyswietlWinyk.setText("X0 = " + (-b / 2 * a));
+                } else {
+                    wyswietlWinyk.setText("Delta jest ujemna");
+                }
+            }
+        });
+    }
+}
+
+
