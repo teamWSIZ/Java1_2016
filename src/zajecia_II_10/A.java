de886b02e2ee1e5fbc303b1fec2ffced53fa6e7d
Stasszek <staszek906@o2.pl>
2017-02-10 18:38:26 +0100

    zaj poczatek 10.02

---
+++ src/zajecia_II_10/A.java
@@ -0,0 +1,19 @@
+package zajecia_II_10;
+
+/**
+ * Created by stasz_000 on 2017-02-10.
+ */
+public class A {
+    public static void main(String[] args) {
+        double pii = 3.141592653589793238;
+
+        double[] kurs = new double[]{100, 110, 111, 130};
+        //dla odważnych
+//        double[] kurs = new double[]{100, 110, 80, 112};
+        double cash = 1000;
+        //wyliczyc ile "cash" mozemy miec na koniec np. po sprzedazy akcji za 130
+
+
+        System.out.println(pii * pii);
+    }
+}
